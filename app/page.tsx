6dbd0b580715eeff975e8ab4352a897131315742
Boris Gratchev <boris.gratchev@sap.com>
2025-02-22 16:44:17 -0800

    components

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,22 @@
+import BookInfo from '@/books/components/BookInfo';
+
 export default function Home() {
   return (
-    <div className='window' style={{width: '300px'}}>
-      <div className='title-bar'>
-        <div className='title-bar-text'>A very cool website</div>
-        <div className='title-bar-controls'>
-          <button aria-label='Minimize'></button>
-          <button aria-label='Maximize'></button>
-          <button aria-label='Close'></button>
+    <>
+      <div className='window' style={{ width: '300px' }}>
+        <div className='title-bar'>
+          <div className='title-bar-text'>A very cool website</div>
+          <div className='title-bar-controls'>
+            <button aria-label='Minimize'></button>
+            <button aria-label='Maximize'></button>
+            <button aria-label='Close'></button>
+          </div>
+        </div>
+        <div className='window-body'>
+          <p>Welcome to Boris Gratchev's website</p>
         </div>
       </div>
-      <div className='window-body'>
-        <p>Welcome to Boris Gratchev's website</p>
-      </div>
-    </div>
+      <BookInfo />
+    </>
   );
 }
