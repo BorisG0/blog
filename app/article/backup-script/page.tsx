f428a068437047bb6f9608dfa5f5668d05f2146e
Boris Gratchev <boris.gratchev@sap.com>
2025-03-22 19:01:29 -0700

    formatting

--- app/article/backup-script/page.tsx
+++ app/article/backup-script/page.tsx
@@ -6,7 +6,6 @@ import { useContext, useEffect } from 'react';
 import { ArticleContext } from '../article-context';
 import './style.css';
 
-
 function BackupScriptPage() {
   const { setDescription } = useContext(ArticleContext);
   useEffect(() => {
@@ -14,14 +13,14 @@ function BackupScriptPage() {
   }, [setDescription]);
   return (
     <>
-      Reading{' '}
-      <Link href={books[0].link || ''} target='_blank'>
-        The Pragramtic Programmer
-      </Link>{' '}
-      I came across the exercise on backing up your laptop. Imagine your laptop is rendered useless in this very moment. How long would it take
-      you to restore it to a state that allows you to continue working?
-      <br />
-      <br />
+      <p>
+        Reading{' '}
+        <Link href={books[0].link || ''} target='_blank'>
+          The Pragramtic Programmer
+        </Link>{' '}
+        I came across the exercise on backing up your laptop. Imagine your laptop is rendered useless in this very moment. How long would it
+        take you to restore it to a state that allows you to continue working?
+      </p>
       Some of the things I would need to restore are:
       <ul>
         <li>installed applications</li>
@@ -29,25 +28,23 @@ function BackupScriptPage() {
         <li>VS Code extensions (although also handled with &apos;Settings Sync&apos;)</li>
       </ul>
       The plan is to compile all of those configurations into a text format that can be stored in a version control system.
-
-      <h1 style={{'fontSize': '16px'}}>Installed Applications</h1>
+      <h1 style={{ fontSize: '16px' }}>Installed Applications</h1>
       <div className='window-body'>
         <pre>ls /Applications {'>'} installed_apps.txt</pre>
       </div>
       <br />
-      <h1 style={{'fontSize': '16px'}}>Homebrew Packages</h1>
+      <h1 style={{ fontSize: '16px' }}>Homebrew Packages</h1>
       <div className='window-body'>
         <pre>brew list {'>'} homebrew_packages.txt</pre>
       </div>
       <br />
-      <h1 style={{'fontSize': '16px'}}>VS Code Extensions</h1>
+      <h1 style={{ fontSize: '16px' }}>VS Code Extensions</h1>
       <div className='window-body'>
         <pre>code --list-extensions {'>'} vscode_extensions.txt</pre>
       </div>
       <br />
-      Put everything into a GitHub repo and you are good to go.
-      <br />
-      To be more advanced, also write a script that does all of the above and pushes to your repo and let it run every day.
+      <p>Put everything into a GitHub repo and you are good to go.</p>
+      <p>To be more advanced, also write a script that does all of the above and pushes to your repo and let it run every day.</p>
     </>
   );
 }
--- app/article/smart-heater/page.tsx
+++ app/article/smart-heater/page.tsx
@@ -1,4 +1,4 @@
-'use client'
+'use client';
 
 import Image from 'next/image';
 import shellyPlugImage from '../../images/shelly_plug.jpg';
@@ -20,11 +20,13 @@ export default function SmartHeaterPage() {
       <p>
         To be able to control when the heater turns on, I had the option of either buying a heater with some smart functionality built in or to
         control its power input programmatically.
-        <br />
+      </p>
+      <p>
         Looking at my I options, I knew that every electric heater has the same efficiency of converting electric energy into heat, so any cheap
         heater should be fine. Anything with smart functionality was always a big step up in price and I was sure any interface on those things
         would be horrible.
-        <br />
+      </p>
+      <p>
         As a programmer, my heating system should be using simple technology, which I can manipulate myself. The Shelly Plug, described itself
         as being to be accessible via HTTP calls.
       </p>
@@ -33,6 +35,10 @@ export default function SmartHeaterPage() {
         <Image src={shellyPlugImage} width={300} height={300} alt='Shelly Plug US' />
       </fieldset>
       <p>Very cool and simple to use product. I am impressed.</p>
+      <p>
+        For the setup I ended up just using the intuitive shelly plug web interface and adding a schedule to have the plug turn on for 30
+        minutes before waking up
+      </p>
     </>
   );
 }
