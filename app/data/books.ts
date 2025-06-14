11168444ae4f3a186e667caa741546db0b09eaad
boris <boris@gratchev.com>
2025-06-14 15:09:44 -0700

    shorten book links

--- app/data/books.ts
+++ app/data/books.ts
@@ -4,117 +4,117 @@ const books: Book[] = [
   {
     title: 'The Pragmatic Programmer',
     author: 'Andrew Hunt and David Thomas',
-    link: 'https://www.amazon.com/Pragmatic-Programmer-journey-mastery-Anniversary/dp/0135957052'
+    link: 'https://www.amazon.com/dp/0135957052'
   },
   {
     title: 'Animal Farm and 1984',
     author: 'George Orwell',
-    link: 'https://www.amazon.com/Animal-Farm-1984-Nineteen-Eighty-Four/dp/0008460981',
+    link: 'https://www.amazon.com/dp/0008460981',
   },
   {
     title: 'American Psycho',
     author: 'Bret Easton Ellis',
-    link: 'https://www.amazon.com/American-Psycho-Bret-Easton-Ellis/dp/0679735771',
+    link: 'https://www.amazon.com/dp/0679735771',
   },
   {
     title: 'Sapiens: A Brief History of Humankind',
     author: 'Yuval Noah Harari',
-    link: 'https://www.amazon.com/Sapiens-Humankind-Yuval-Noah-Harari/dp/0062316117',
+    link: 'https://www.amazon.com/dp/0062316117',
   },
   {
     title: 'Homo Deus: A Brief History of Tomorrow',
     author: 'Yuval Noah Harari',
-    link: 'https://www.amazon.com/Homo-Deus-Brief-History-Tomorrow/dp/0062464345',
+    link: 'https://www.amazon.com/dp/0062464345',
   },
   {
     title: 'A brief history of time',
     author: 'Stephen Hawking',
-    link: 'https://www.amazon.com/Brief-History-Time-Stephen-Hawking/dp/0553380168',
+    link: 'https://www.amazon.com/dp/0553380168',
   },
   {
     title: 'Atlas Shrugged',
     author: 'Ayn Rand',
-    link: 'https://www.amazon.com/Atlas-Shrugged-Ayn-Rand/dp/0451191145',
+    link: 'https://www.amazon.com/dp/0451191145',
   },
   {
     title: 'Thinking, Fast and Slow',
     author: 'Daniel Kahneman',
-    link: 'https://www.amazon.com/Thinking-Fast-Slow-Daniel-Kahneman/dp/0374533555',
+    link: 'https://www.amazon.com/dp/0374533555',
   },
   {
     title: 'The 4 hour work week',
     author: 'Timothy Ferriss',
-    link: 'https://www.amazon.com/4-Hour-Workweek-Escape-Live-Anywhere/dp/0307465357',
+    link: 'https://www.amazon.com/dp/0307465357',
   },
   {
     title: 'Atomic Habits',
     author: 'James Clear',
-    link: 'https://www.amazon.com/Atomic-Habits-Proven-Build-Break/dp/0735211299'
+    link: 'https://www.amazon.com/dp/0735211299'
   },
   {
     title: '12 Rules for Life',
     author: 'Jordan Peterson',
-    link: 'https://www.amazon.com/12-Rules-Life-Antidote-Chaos/dp/0345816021',
+    link: 'https://www.amazon.com/dp/0345816021',
   },
   {
     title: 'Beyond Order',
     author: 'Jordan Peterson',
-    link: 'https://www.amazon.com/Beyond-Order-More-Rules-Life/dp/0593084640'
+    link: 'https://www.amazon.com/dp/0593084640'
   },
   {
     title: 'The Snail on the Slope',
     author: 'Arkady andBoris Strugatsky',
-    link: 'https://www.amazon.com/Snail-Slope-Rediscovered-Classics/dp/1613737548'
+    link: 'https://www.amazon.com/dp/1613737548'
   },
   {
     title: 'How to Win Friends and Influence People',
     author: 'Dale Carnegie',
-    link: 'https://www.amazon.com/How-Win-Friends-Influence-People/dp/0671027034'
+    link: 'https://www.amazon.com/dp/0671027034'
   },
   {
     title: 'Humble Pi: A Comedy of Maths Errors',
     author: 'Matt Parker',
-    link: 'https://www.amazon.com/Humble-Pi-Comedy-Maths-Errors/dp/0141989149'
+    link: 'https://www.amazon.com/dp/0141989149'
   },
   {
     title: 'Treasure Island',
     author: 'Robert Louis Stevenson',
-    link: 'https://www.amazon.com/Treasure-Island-Signet-Classics-Stevenson/dp/1101990325'
+    link: 'https://www.amazon.com/dp/1101990325'
   },
   {
     title: 'Die Verwandlung',
     author: 'Franz Kafka',
-    link: 'https://www.amazon.com/Die-Verwandlung-Format-Taschenbuch-German/dp/9982470906'
+    link: 'https://www.amazon.com/dp/9982470906'
   },
   {
     title: 'White Nights',
     author: 'Fyodor Dostoevsky',
-    link: 'https://www.amazon.com/White-Nights-Fyodor-Dostoevsky/dp/1548269352'
+    link: 'https://www.amazon.com/dp/1548269352'
   },
   {
     title: 'Crime and Punishment',
     author: 'Fyodor Dostoevsky',
-    link: 'https://www.amazon.com/Punishment-Signet-Classics-Fyodor-Dostoyevsky/dp/0451530063'
+    link: 'https://www.amazon.com/dp/0451530063'
   },
   {
     title: 'Wilhelm Tell',
     author: 'Friedrich Schiller',
-    link: 'https://www.amazon.com/Wilhelm-Tell-German-Friedrich-Schiller/dp/1169129528'
+    link: 'https://www.amazon.com/dp/1169129528'
   },
   {
     title: 'Faust - Part One',
     author: 'Johann Wolfgang von Goethe',
-    link: 'https://www.amazon.com/Goethes-Faust-Johann-Wolfgang-Goethe/dp/0385031149'
+    link: 'https://www.amazon.com/dp/0385031149'
   },
   {
     title: 'Nathan der Weise',
     author: 'Gotthold Ephraim Lessing',
-    link: 'https://www.amazon.com/Nathan-Weise-dramatisches-Aufz%C3%BCgen-Gymnasiale/dp/1986504700'
+    link: 'https://www.amazon.com/dp/1986504700'
   },
   {
     title: 'Romeo and Juliet',
     author: 'William Shakespeare',
-    link: 'https://www.amazon.com/Romeo-Juliet-Folger-Shakespeare-Library/dp/0743477111'
+    link: 'https://www.amazon.com/dp/0743477111'
   },
   {
     title: 'The Adventures of Tom Sawyer',
